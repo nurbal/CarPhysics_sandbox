3c79d69bcba78fa180a0fbd81149aa156edef8a3
Bruno Carrez <bruno.carrez@mila.quebec>
2022-05-25 09:32:06 -0400

    Initial commit

---
+++ collision_bench.py
@@ -0,0 +1,192 @@
+# from Box2D.examples.framework import (Framework, main)
+
+from concurrent.futures.thread import _global_shutdown_lock
+from tkinter import E
+from Box2D import *
+from Box2D.examples.framework import (Framework, Keys, main)
+from random import uniform
+
+from time import time
+
+class Benchmark :
+
+    # moving car (no free move)
+    class MovingCar :
+        def __init__(self,world,initialpos,endpos,speed,initialtrip=0.0):
+            self.initialpos = b2Vec2(initialpos)
+            self.direction = b2Vec2(endpos) - self.initialpos
+            self.tripLen = self.direction.Normalize()
+            self.speed = speed
+            self.trip = initialtrip
+            self.world = world
+
+            carShape = b2PolygonShape(box=(1,2))
+            boxFD = b2FixtureDef(
+                shape=carShape,
+                friction=0.2,
+                density=20,
+            )
+            self.body = self.world.CreateBody(
+                position=self.initialpos,
+                fixtures=boxFD,
+            )
+            # TODO: orienter le body selon self.direction
+
+        def Step(self,timeStep):
+            self.trip += self.speed * timeStep
+            if self.trip > self.tripLen:
+                self.trip = 0.0
+            self.body.position = self.initialpos + (self.direction * self.trip)
+            self.body.linearVelocity = self.direction*self.speed
+            self.body.angularVelocity = 0.0
+            
+    class FreeCar :
+        def __init__(self,world,spawnDistance,maxDistance):
+            self.world = world
+            self.spawnDistance = spawnDistance
+            self.maxDistance = maxDistance
+
+            carShape = b2PolygonShape(box=(3,3))
+            boxFD = b2FixtureDef(
+                shape=carShape,
+                friction=0.2,
+                density=20,
+            )
+            self.body = self.world.CreateDynamicBody(
+                position=(
+                    uniform(-self.spawnDistance,self.spawnDistance),
+                    uniform(-self.spawnDistance,self.spawnDistance),
+                ),
+                fixtures=boxFD,
+            )
+            # TODO: orienter le body selon self.direction
+
+        def Step(self,timeStep):
+            if self.body.position.length > self.maxDistance :
+                self.body.position = (
+                    uniform(-self.spawnDistance,self.spawnDistance),
+                    uniform(-self.spawnDistance,self.spawnDistance),
+                )
+
+
+
+            return
+            # self.trip += self.speed * timeStep
+            # if self.trip > self.tripLen:
+            #     self.trip = 0.0
+            # self.body.position = self.initialpos + (self.direction * self.trip)
+            
+
+
+
+    def __init__(self,world):
+        self.world = world
+
+        # flat world: no gravity here
+        world.gravity=(0,0)
+
+        # fixed obstacles :
+
+        # cross intersection shape
+        w = 9  # half-road width
+        l = 30 # half road length
+        #   A B
+        # K L C D
+        # J I F E
+        #   H G
+        A = (-w,l)
+        B = (w,l)
+        C = (w,w)
+        D = (l,w)
+        E = (l,-w)
+        F = (w,-w)
+        G = (w,-l)
+        H = (-w,-l)
+        I = (-w,-w)
+        J = (-l,-w)
+        K = (-l,w)
+        L = (-w,w)
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[A,B]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[B,C]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[C,D]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[D,E]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[E,F]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[F,G]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[G,H]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[H,I]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[I,J]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[J,K]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[K,L]))
+        self.world.CreateBody(shapes=b2EdgeShape(vertices=[L,A]))
+
+        carShape = b2PolygonShape(box=(1,2))
+        boxFD = b2FixtureDef(
+            shape=carShape,
+            friction=0.2,
+            density=20,
+        )
+
+        for x in range(1):
+            for y in range(1):
+                body = self.world.CreateDynamicBody(
+                # body = self.world.CreateBody(
+                    position=(x*4, y*6),
+                    fixtures=boxFD,
+                )
+
+        self.cars = []
+        for initialtrip in [0.0,l-3]:
+            self.cars.append(self.MovingCar(world=self.world,initialpos=(-w*0.75,l-3),endpos=(-w*0.75,-l+3),speed=15,initialtrip=initialtrip))
+            self.cars.append(self.MovingCar(world=self.world,initialpos=(-w*0.25,l-3),endpos=(-w*0.25,-l+3),speed=25,initialtrip=initialtrip))
+            self.cars.append(self.MovingCar(world=self.world,initialpos=(w*0.75,-l+3),endpos=(w*0.75,l-3),speed=15,initialtrip=initialtrip))
+            self.cars.append(self.MovingCar(world=self.world,initialpos=(w*0.25,-l+3),endpos=(w*0.25,l-3),speed=25,initialtrip=initialtrip))
+ 
+            self.cars.append(self.MovingCar(world=self.world,initialpos=(l-3,-w*0.75),endpos=(-l+3,-w*0.75),speed=15,initialtrip=initialtrip+(l-3)/2))
+            self.cars.append(self.MovingCar(world=self.world,initialpos=(l-3,-w*0.25),endpos=(-l+3,-w*0.25),speed=25,initialtrip=initialtrip+(l-3)/2))
+            self.cars.append(self.MovingCar(world=self.world,initialpos=(-l+3,w*0.75),endpos=(l-3,w*0.75),speed=15,initialtrip=initialtrip+(l-3)/2))
+            self.cars.append(self.MovingCar(world=self.world,initialpos=(-l+3,w*0.25),endpos=(l-3,w*0.25),speed=25,initialtrip=initialtrip+(l-3)/2))
+
+        self.cars.append(self.FreeCar(self.world,w/4,w*2))
+
+    def Step(self,timeStep):
+        # timeStep in seconds
+        for car in self.cars:
+            car.Step(timeStep)
+        return
+
+
+class BenchFramework (Framework):
+    name = "Collision benchmark framework"
+    # description = "Press j to toggle the rope joint."
+
+    def __init__(self):
+        super(BenchFramework, self).__init__()
+
+        # visual settings...
+        self.settings.pause = True
+        # self.settings.drawMenu = False
+
+        # create a benchmark... 
+        self.benchmark = Benchmark(self.world)
+
+    def Step(self, settings):
+        super(BenchFramework, self).Step(settings)
+
+        # Don't do anything if the setting's Hz are <= 0
+        if not settings.pause:
+            if settings.hz > 0.0:
+                timeStep = 1.0 / settings.hz
+                self.benchmark.Step(timeStep)
+
+if __name__ == "__main__":
+    # main(BenchFramework)
+
+    world = b2World()
+    bench = Benchmark(world)
+    nbSteps = 100000
+    t0 = time()
+    for i in range(nbSteps):
+        bench.Step(1.0/60)
+    dt = time() - t0
+    freq = float(nbSteps) / dt
+    print(f"Steps={nbSteps} Freq={freq}")
